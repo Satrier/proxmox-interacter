e01ecccf5e20e5e50c6b1e18ee3fe0d44851d4bf
Raul Sampedro <5142014+rsrdesarrollo@users.noreply.github.com>
2026-02-02 22:48:11 +0100

    clean templates, move info to other command

--- pkg/app/about.go
+++ pkg/app/about.go
@@ -6,16 +6,16 @@ import (
 	tele "gopkg.in/telebot.v3"
 )
 
-func (a *App) HandleInfo(c tele.Context) error {
+func (a *App) HandleAbout(c tele.Context) error {
 	a.Logger.Info().
 		Int64("sender_id", c.Sender().ID).
 		Str("sender", c.Sender().Username).
 		Str("text", c.Text()).
 		Msg("Got info query")
 
-	template, err := a.TemplateManager.Render("info", a.Version)
+	template, err := a.TemplateManager.Render("about", a.Version)
 	if err != nil {
-		a.Logger.Error().Err(err).Msg("Error rendering info template")
+		a.Logger.Error().Err(err).Msg("Error rendering about template")
 		return c.Reply(fmt.Sprintf("Error rendering template: %s", err))
 	}
 
